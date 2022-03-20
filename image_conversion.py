bd1e3d8f93101afdfdb284b322ce42ecc6f22a7b
Mirai Niki <mirainiki@MirainoMac-mini.local>
2022-03-20 22:04:15 +0900

    画像処理のコードを追加

---
+++ image_conversion.py
@@ -0,0 +1,16 @@
+from statistics import quantiles
+from PIL import Image, ImageFilter
+import sys
+
+if len(sys.argv) != 2:
+    print("引数は1つ指定してください")
+    exit()
+
+img_path = sys.argv[1]
+org_img = Image.open(img_path)
+
+converted_img = org_img.convert('L').filter(ImageFilter.GaussianBlur())
+# converted_img.show()
+
+converted_img_path = '.'.join(img_path.split(".")[0:-1]) + "_conv.jpg"
+converted_img.save(converted_img_path, quantiles=95)
