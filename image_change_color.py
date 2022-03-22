e4719dbd81d40fa1e297975cca930722478b439b
minngk <taiyakihajinsei3@gmail.com>
2022-03-22 22:30:35 +0900

    lintを追加

--- image_change_color.py
+++ image_change_color.py
@@ -1,18 +1,21 @@
 # 引数でPATHを受け取り画像を色変換したものと並べる
 # 参考：https://pillow.readthedocs.io/en/stable/handbook/tutorial.html#processing-individual-bands
 
-from PIL import Image, ImageFilter
+from PIL import Image
 import sys
+
 
 def get_concat_v(img1, img2):
     dst = Image.new('RGB', (img1.width, img1.height + img2.height))
-    dst.paste(img1, (0,0))
+    dst.paste(img1, (0, 0))
     dst.paste(img2, (0, img1.height))
     return dst
+
 
 def resize_to_width(img, width):
     height = round(width*img.height/img.width)
     return img.resize((width, height))
+
 
 if len(sys.argv) != 2:
     print("引数は1つ指定してください")
@@ -21,7 +24,7 @@ if len(sys.argv) != 2:
 img_path = sys.argv[1]
 org_img = resize_to_width(Image.open(img_path), 128)
 new_img = org_img.copy()
-rgb_dict = {"R":0, "G":1, "B":2}
+
 for i in range(3):
     # split the image into individual bands
     source = org_img.split()
@@ -42,4 +45,3 @@ for i in range(3):
 
 new_img_path = '.'.join(img_path.split(".")[0:-1]) + "_change_color.jpg"
 new_img.save(new_img_path, quantiles=95)
-
